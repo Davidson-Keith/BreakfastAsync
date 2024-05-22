7065a3b4275936134aca6f9c11ff09fdfd90268f
Keith Davidson <davidson.keith@gmail.com>
2024-05-23 08:46:13 +1000

    Improve task waiting.

--- BreakfastAsync/Program.cs
+++ BreakfastAsync/Program.cs
@@ -8,16 +8,17 @@ namespace BreakfastAsync {
   class Program {
     static async Task Main(String[] args) {
       new Coffee().PourCoffee();
-      var eggsTask = new Eggs(2).FryEggsAsync();
-      var baconTask = new Bacon(3).FryBaconAsync();
-      var toastTask = new Toast(2).MakeToastWithButterAndJamAsync();
-      var breakfastTasks = new List<Task> { eggsTask, baconTask, toastTask };
-      while (breakfastTasks.Count > 0) {
-        Task finishedTask = await Task.WhenAny(breakfastTasks);
-        await finishedTask;
-        breakfastTasks.Remove(finishedTask);
-      }
-
+      Task eggsTask = new Eggs(2).FryEggsAsync();
+      Task baconTask = new Bacon(3).FryBaconAsync();
+      Task toastTask = new Toast(2).MakeToastWithButterAndJamAsync();
+      Task.WaitAll([eggsTask, baconTask, toastTask]);
+      // This is how they did it in the tutorial, but the single line above does the same.
+      // List<Task> breakfastTasks = [eggsTask, baconTask, toastTask];
+      // while (breakfastTasks.Count > 0) {
+      //   Task finishedTask = await Task.WhenAny(breakfastTasks);
+      //   // await finishedTask;
+      //   breakfastTasks.Remove(finishedTask);
+      // }
       new Juice().PourOJ();
       Console.WriteLine("Breakfast is ready!");
     }
